5b074d147c705dc340f342584b06e7444fa7975e
cheiy <gschege@gmail.com>
2022-10-14 18:48:43 +0000

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - Prints out a statement using the puts() function
+ *
+ * Description: Prints out a statement using the puts() function.
+ * Return: 0
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+
+	return (0);
+}
