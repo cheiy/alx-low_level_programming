307e14252c63b6b9b63864bcd32b57de0c5a63a9
cheiy <gschege@gmail.com>
2022-10-14 18:57:05 +0000

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - prints a statement using printf() function
+ *
+ * Description: Program prints out a statement using printf() function
+ *
+ * Return: returns 0 if successfully executed
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
