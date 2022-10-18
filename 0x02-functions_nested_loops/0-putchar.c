f5ea4d6aeb316f6197cadc95f62f1b37313ac5ff
cheiy <gschege@gmail.com>
2022-10-18 16:35:45 +0000

    Tasks

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,23 @@
 #include "main.h"
+/**
+ * main - prints the word _putchar
+ *
+ * Description: prints the word _putchar using a custom function
+ *
+ * Return: 0 if it runs successfully
+ */
 
 int main(void)
 {
-	int our_chars[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int our_chars[] = {95, 112, 117, 116, 99, 104, 97, 114, 10};
 	int count = 0;
 
-	while (count < 8 )
+	while (count < 9)
 	{
 		_putchar(our_chars[count]);
 		count++;
 	}
 
-	/*_putchar(our_chars[0]);*/
 
 	return (0);
 }
