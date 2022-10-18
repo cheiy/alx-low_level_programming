ee6bff18ff6050d2e5e2c306b398aab79451707f
cheiy <gschege@gmail.com>
2022-10-18 23:33:28 +0000

    I'm federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,16 +12,34 @@ void jack_bauer(void)
 	int loop1;
 	int loop2;
 
-	loop1 = 0;
-	loop2 = 0;
-	while (loop1 < 24)
+	loop1 = 48;
+	loop2 = 48;
+	while (loop1 < 50)
 	{
-		while (loop2 < 60)
+		while (loop2 < 51)
 		{
-			_putchar(loop2);
+			while (loop3 < 53)
+			{
+				while (loop4 < 57)
+				{
+					_putchar(loop1);
+					_putchar(loop2);
+					_putchar(loop3);
+					_putchar(loop4);
+					loop4++;
+				}
+				loop4 = 48;
+				loop3++;
+			}
+			loop4 = 48;
+			loop4 = 48;
 			loop2++;
+			_putchar(58);
 		}
-		loop2 = 0;
+		loop4 = 48;
+		loop3 = 48;
+		loop2 = 48;
 		loop1++;
 	}
+
 }
