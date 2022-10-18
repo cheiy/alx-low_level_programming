a4ffba984d500f6ab11bba8e4813359880a8216a
cheiy <gschege@gmail.com>
2022-10-18 22:27:46 +0000

    here are only 3 colors, 10 digits, and 7 notes; it's what we do with them that's important

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Function prints the last digit of a decimal number
+ * @num: The number whose last digit we want to calculate
+ *
+ * Description: - Program prints last digit of a decimal number
+ *
+ * Return: Returns the last digit of a given number
+ */
+int print_last_digit(int num)
+{
+	int last_digit;
+
+	last_digit = num % 10;
+
+	return (last_digit);
+}
