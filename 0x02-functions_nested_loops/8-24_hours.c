8ef23e70089349b83fa9170c24468d92a77e9996
cheiy <gschege@gmail.com>
2022-10-19 08:26:44 -0700

    Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,51 +1,22 @@
-#include "main.h"
-/**
- * jack_bauer - prints every minute of the day in 24
- *
- * Description: prints every minute of the day in 24
- *
- * Return: void
- *
- */
+
 void jack_bauer(void)
 {
-	int loop1, loop2, loop3, loop4;
-
-	int hours_1[] = {48, 49, 50};
-	int hours_2[] = {48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 49, 50, 51, 53, \
-	54, 55, 56, 57, 48, 49, 50, 51};
-	int minutes_1[] = {48, 49, 50, 51, 52, 53};
-	int minutes_2[] = {48, 49, 50, 51, 52, 53, 54, 55, 56, 57};
-
-
-	loop1 = loop2 = loop3 = loop4 = 0;
-	while (loop1 < 2)
-	{
-		while (loop2 < 24)
+	int hr;
+	int min;
+	hr = 0;
+	min  = 0;
+	while (hr <= 24)
+	{	
+		while( min < = 59)
 		{
-			while (loop3 < 6)
-			{
-				while (loop4 < 10)
-				{
-					_putchar(hours_1[loop1]);
-					_putchar(hours_2[loop2]);
-					_putchar(58);
-					_putchar(minutes_1[loop3]);
-					_putchar(minutes_2[loop4]);
-					_putchar('\n');
-					loop4++;
-				}
-				loop4 = 0;
-				loop3++;
-			}
-			loop4 = 0;
-			loop3 = 0;
-			loop2++;
+			_putchar(( hr / 10 ) + '0');
+			_putchar(( hr % 10 ) + '0');
+			_putchar(':');
+			_putchar(( min / 10 ) + '0');
+			_putchar(( min % 10 ) + '0');
+			_putchar('\n');
+			min++;
 		}
-		loop4 = 0;
-		loop3 = 0;
-		loop2 = 0;
-		loop1++;
+		hr++;
 	}
-}		
-				
+}
