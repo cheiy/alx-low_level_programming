bff6a6aded559005e729a0d733cfb188b0cce63e
cheiy <gschege@gmail.com>
2022-10-19 08:36:17 -0700

    Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,19 +1,31 @@
+#include  "main.h"
+
+/**
+ * jack_bauer - Prints 24 hr themed time
+ *
+ * Description: PRints 24 hour themed time
+ *
+ * Return: void
+ */
+
 
 void jack_bauer(void)
 {
 	int hr;
 	int min;
+
 	hr = 0;
 	min  = 0;
+
 	while (hr <= 24)
-	{	
-		while( min < = 59)
+	{
+		while (min <= 59)
 		{
-			_putchar(( hr / 10 ) + '0');
-			_putchar(( hr % 10 ) + '0');
+			_putchar((hr / 10) + '0');
+			_putchar((hr % 10) + '0');
 			_putchar(':');
-			_putchar(( min / 10 ) + '0');
-			_putchar(( min % 10 ) + '0');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
 			_putchar('\n');
 			min++;
 		}
