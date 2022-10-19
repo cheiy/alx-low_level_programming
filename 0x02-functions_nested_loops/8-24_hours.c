27d410ab653c0538a9539b460c6333ffab0b5ac5
cheiy <gschege@gmail.com>
2022-10-19 00:02:47 +0000

    I'm federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,20 +9,15 @@
  */
 void jack_bauer(void)
 {
-	int loop1;
-	int loop2;
-	int loop3;
-	int loop4;
+	int loop1, loop2, loop3, loop4;
+
 	int hours_1[] = {48, 49, 50};
 	int hours_2[] = {48, 49, 50, 51, 52, 53, 54, 55, 56, 57};
 	int minutes_1[] = {48, 49, 50, 51, 52, 53};
 	int minutes_2[] = {48, 49, 50, 51, 52, 53, 54, 55, 56, 57};
 
 
-	loop1 = 0;
-	loop2 = 0;
-	loop3 = 0;
-	loop4 = 0;
+	loop1 = loop2 = loop3 = loop4 = 0;
 	while (loop1 < 3)
 	{
 		while (loop2 < 9)
@@ -36,6 +31,7 @@ void jack_bauer(void)
 					_putchar(58);
 					_putchar(minutes_1[loop3]);
 					_putchar(minutes_2[loop4]);
+					_putchar('\n');
 					loop4++;
 				}
 				loop4 = 0;
