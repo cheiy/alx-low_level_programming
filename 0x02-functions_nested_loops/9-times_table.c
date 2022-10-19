04cf9e6500eb5ec0c20acc9439b5d8ca40735f44
cheiy <gschege@gmail.com>
2022-10-19 09:23:18 -0700

    Times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,9 +13,9 @@ void times_table(void)
 	int j;
 	int t;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j < 9; j++)
+		for (j = 0; j <= 9; j++)
 		{
 			t = i * j;
 			_putchar(t + '0');
