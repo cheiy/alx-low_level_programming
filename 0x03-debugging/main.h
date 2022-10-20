d270fe71db47474bcaac93e904fefaa7ad2e7812
cheiy <gschege@gmail.com>
2022-10-20 15:36:46 +0000

    Leap Year

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -7,4 +7,8 @@ void positive_or_negative(int i);
 
 int largest_number(int c, int h, int e);
 
+int convert_day(int m, int d);
+
+void print_remaining_days(int m, int d, int y);
+
 #endif /* MAIN_H */
