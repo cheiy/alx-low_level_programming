43ca1f5406a57418bcbef6ae9b7bb403204e3b19
cheiy <gschege@gmail.com>
2022-10-22 15:56:33 +0000

    I feel like I am diagonally....

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,24 +1,24 @@
 #include "main.h"
 /**
  * print_diagonal - Prints a diagonal line
- * @size: base of the 'virtual triangle'
+ * @n: base of the 'virtual triangle'
  *
  * Description: Prints a diagonal line  when given a size
  * which will be base.
  *
  * Return: nothing
  */
-void print_diagonal(int size)
+void print_diagonal(int n)
 {
 	int b;
 	int i;
 	int base;
 	int height;
 
-	height = size;
+	height = n;
 	base = 0;
 
-	if (size <= 0)
+	if (n <= 0)
 	{
 		_putchar('\n');
 	}
