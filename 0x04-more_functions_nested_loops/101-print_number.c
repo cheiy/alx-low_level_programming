2ea6ec3e1c47290a43dbcff811c47aca03641191
cheiy <gschege@gmail.com>
2022-10-23 16:26:45 +0000

    Numbers have life..

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,54 +10,22 @@
  */
 void print_number(int n)
 {
-	int num, rem;
+	int num;
 
 	num = n;
 	if (num < 0)
 	{
 		_putchar('-');
 		num = -num;
-		_putchar(num / 10 + '0');
-		rem = (num % 10);
-		_putchar(rem + '0');
 	}
-	else if (num < 10)
+	if (num == 0)
 	{
-		_putchar(n + '0');
+		_putchar('0');
 	}
-	else if (num >= 10000)
+
+	if (num / 10)
 	{
-		_putchar(num / 10000 + '0');
-		rem = (num % 10000);
-		_putchar(rem / 1000 + '0');
-		rem = (num % 1000);
-		_putchar(rem / 100 + '0');
-		rem = (rem % 100);
-		_putchar(rem / 10 + '0');
-		rem = (rem % 10);
-		_putchar(rem + '0');
+		print_number(num / 10);
 	}
-	else if (num >= 1000)
-	{
-		_putchar(num / 1000 + '0');
-		rem = (num % 1000);
-		_putchar(rem / 100 + '0');
-		rem = (rem % 100);
-		_putchar(rem / 10 + '0');
-		rem = (rem % 10);
-		_putchar(rem + '0');
-	}
-	else if (num < 100)
-	{
-		_putchar(num / 10 + '0');
-		rem = (num % 10);
-		_putchar(rem + '0');
-	}
-	else if (num > 100)
-	{
-		_putchar(num / 100 + '0');
-		rem = (num % 100);
-		_putchar(rem / 100 + '0');
-		_putchar(rem % 10 + '0');
-	}
+	_putchar(num % 10 + '0');
 }
