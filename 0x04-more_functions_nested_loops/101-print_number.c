027e697740cd1d2af7cd39f87c60121b49108ced
cheiy <gschege@gmail.com>
2022-10-23 15:46:44 +0000

    Numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -25,6 +25,18 @@ void print_number(int n)
 	{
 		_putchar(n + '0');
 	}
+	else if (num >= 10000)
+	{
+		_putchar(num / 10000 + '0');
+		rem = (num % 10000);
+		_putchar(rem / 1000 + '0');
+		rem = (num % 1000);
+		_putchar(rem / 100 + '0');
+		rem = (rem % 100);
+		_putchar(rem / 10 + '0');
+		rem = (rem % 10);
+		_putchar(rem + '0');
+	}
 	else if (num >= 1000)
 	{
 		_putchar(num / 1000 + '0');
