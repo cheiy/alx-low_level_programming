d5d1592b48b17519fe69960cccd40dac0fbd8d57
cheiy <gschege@gmail.com>
2022-10-27 02:51:48 +0000

    Don't hate the hacker

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Program generates random valid passwords for the program
+ * 101-crackme
+ *
+ * Description - Program generates random valid passwods for
+ * 101-crackme program
+ *
+ * Return: 0 always.
+ */
+int main(void)
+{
+	return (0);
+}
