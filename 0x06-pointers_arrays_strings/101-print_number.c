84a218ad9d791d820034f83746ce16037965485c
cheiy <gschege@gmail.com>
2022-10-28 20:56:22 +0000

    Numbers have life..

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_number - Function prints an integer using only the _putchar function
+ * @n: The integer to print
+ *
+ * Description: Function prints an integer using only the _putchar function.
+ *
+ * Return: Nothing.
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+	if (n == 0)
+	{
+		_putchar(n);
+	}
+	if (n / 10)
+	{
+		print_number(n / 10);
+	}
+	_putchar(n % 10 + '0');
+}
