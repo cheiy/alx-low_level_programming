8549cf0c57168b9b48fd47bd32a45b0f917534dd
cheiy <gschege@gmail.com>
2022-10-28 18:13:50 +0000

    Mozart composed ....

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * leet - Function encodes a string into 1337.
+ * @str: Pointer to the string you want to encode.
+ *
+ * Description: Function encodes a string into 1337.
+ *
+ * Return: Pointer to the encoded string.
+ */
+char *leet(char *str)
+{
+	char *cases = "AEOTLaeotl";
+	char *leet = "4307143071";
+	int count;
+	int len;
+
+	count = 0;
+	len = 0;
+	while (str[len] != '\0')
+	{
+		while (count < 10)
+		{
+			if (str[len] == cases[count])
+			{
+				str[len] = leet[count];
+				break;
+			}
+			else
+			{
+				count++;
+			}
+		}
+		count = 0;
+		len++;
+	}
+	return (str);
+}
