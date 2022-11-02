13a88bd3adc2fd527b30a049a41a7f16c6fed6e4
cheiy <gschege@gmail.com>
2022-11-02 02:57:30 +0000

    Chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * print_chessboard - Prints the chessboard.
+ * @a: pointer to array holding chess pieces.
+ *
+ * Description: Prints the chessboard.
+ *
+ * Return: Nothing.
+ */
+void print_chessboard(char (*a)[8])
+{
+	int row, col;
+
+	row = 0;
+	col = 0;
+	while (row < 8)
+	{
+		while (col < 8)
+		{
+			if (col == 7)
+			{
+				_putchar(a[row][col]);
+				_putchar('\n');
+			}
+			else
+			{
+				_putchar(a[row][col]);
+			}
+			col++;
+		}
+		row++;
+	}
+}
+
