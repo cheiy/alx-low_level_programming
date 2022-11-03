287a8a62afac04e971c399b2d53b14ab81876cbc
cheiy <gschege@gmail.com>
2022-11-03 12:28:03 +0000

    Recursive _puts

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -11,12 +11,12 @@ void _puts_recursion(char *s)
 {
 	_putchar(*s);
 	s++;
-	if(*s != '\0')
+	if (*s != '\0')
 	{
-		_puts_recursion (s);
+		_puts_recursion(s);
 	}
 	else
 	{
- 		_putchar('\n');
+		_putchar('\n');
 	}
 }
