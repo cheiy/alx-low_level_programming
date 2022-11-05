4b54c99a1ca7674947e015d6ad920522e92db171
cheiy <gschege@gmail.com>
2022-11-05 11:56:56 +0000

    100_times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -17,9 +17,9 @@ void print_times_table(int n)
 	j = 0;
 	if (n > 0 && n <= 15)
 	{
-		for (i = 0; i < n; i++)
+		for (i = 0; i <= n; i++)
 		{
-			for (j = 0; j < n; j++)
+			for (j = 0; j <= n; j++)
 			{
 				if (j < n - 1)
 				{
