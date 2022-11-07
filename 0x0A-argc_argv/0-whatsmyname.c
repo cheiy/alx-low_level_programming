d5b9ca703a682103b25f3fc0881024c6881eb4b2
cheiy <gschege@gmail.com>
2022-11-07 17:40:24 +0000

    Whatsmyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,6 @@
  * @argv: Array of pointers to the arguments passed to main.
  *
  * Description: Program prints its name followed by a new line.
- *
  * Return: 0 if run successfully, 1 otherwise.
  */
 int main(int argc __attribute__((unused)), char *argv[])
