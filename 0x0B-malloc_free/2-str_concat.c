d01ca1a2deadc279f84412f8e05ea18a1b523aa3
cheiy <gschege@gmail.com>
2022-11-08 20:46:18 +0000

    concatenate

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,6 +19,14 @@ char *str_concat(char *s1, char *s2)
 
 	s1_len = 0;
 	s2_len = 0;
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
 	while (s1[s1_len])
 	{
 		s1_len++;
