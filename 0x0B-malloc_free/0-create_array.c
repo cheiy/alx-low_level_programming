bf54d1a903e10533c544e26350bff639e926c3de
cheiy <gschege@gmail.com>
2022-11-08 13:05:01 +0000

    malloc,free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,28 +17,10 @@ char *create_array(unsigned int size, char c)
 	char *arr;
 	unsigned int i;
 
-	/*
-	 * Try allocating memory
-	 */
 	arr = (char *) (malloc(sizeof(char) * size));
-	if (arr == NULL)
+	for (i = 0; i < size; i++)
 	{
-		/*
-		 * If it fails, print this message
-		 */
-		printf("Unable to allocate memory of size %d\n", size);
-	}
-	else
-	{
-		/*
-		 * Initialize the memory allocated with char c
-		 */
-		for (i = 0; i < size; i++)
-		{
-			arr[i] = c;
-		}
+		arr[i] = c;
 	}
 	return (arr);
 }
-
-
