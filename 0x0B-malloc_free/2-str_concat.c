63bd7a0951313fc47e3e66018f6112931442c9d9
cheiy <gschege@gmail.com>
2022-11-08 20:57:35 +0000

    concatenate

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,51 +13,37 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *s1_s2;
-	unsigned int s1_len;
-	unsigned int s2_len;
-	unsigned int total_len;
+	unsigned int s1_len, s2_len, total_len;
 
-	s1_len = 0;
-	s2_len = 0;
+	s1_len = s2_len = 0;
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 	while (s1[s1_len])
-	{
 		s1_len++;
-	}
+
 	while (s2[s2_len])
-	{
 		s2_len++;
-	}
+
 	total_len = s1_len + s2_len + 1;
 	s1_s2 = (char *)(malloc(sizeof(char) * total_len));
 	if (s1_s2 == NULL)
-	{
 		return (NULL);
-	}
-	else
+	s1_len = 0;
+	while (s1[s1_len] != '\0')
 	{
-		s1_len = 0;
-		while (s1[s1_len] != '\0')
-		{
-			s1_s2[s1_len] = s1[s1_len];
-			s1_len++;
-		}
-		s2_len = 0;
-		s2_len = s1_len;
-		s1_len = 0;
-		while (s2[s1_len] != '\0' && s2_len < total_len)
-		{
-			s1_s2[s2_len] = s2[s1_len];
-			s2_len++;
-			s1_len++;
-		}
+		s1_s2[s1_len] = s1[s1_len];
+		s1_len++;
+	}
+	s2_len = 0;
+	s2_len = s1_len;
+	s1_len = 0;
+	while (s2[s1_len] != '\0' && s2_len < total_len)
+	{
+		s1_s2[s2_len] = s2[s1_len];
+		s2_len++;
+		s1_len++;
 	}
 	return (s1_s2);
 }
