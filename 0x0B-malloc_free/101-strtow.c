1f7329022f3d4a49bafa39088a355ae34b0a2eb3
cheiy <gschege@gmail.com>
2022-11-11 02:38:18 +0000

    split

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * strtow - Function returns a pointer to an array of strings (words).
+ * @str: The string to split
+ *
+ * Description: Function returns a pointer to an array of strings
+ *
+ * Return: Pointer to an array of strings.
+ */
+char **strtow(char *str)
+{
+	char **strs;
+	int len;
+
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	if (str == NULL || str == 0)
+		return (NULL);
+	strs = (char **)malloc(sizeof(char *) * len - 2);
+	if (strs == NULL)
+		return (NULL);
+	while (*str != '\0')
+	{
+		if (*str != 32)
+		{
+			*strs = str;
+		}
+		str++;
+	}
+
+	return (strs);
+}
