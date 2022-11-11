5090e4269b66ea43ac8ab288a751c77a68e6839d
cheiy <gschege@gmail.com>
2022-11-11 18:03:57 +0000

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,7 +13,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *arr;
 
-	if (nmemb == 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 	arr = malloc(nmemb * sizeof(size));
 	if (arr == NULL)
