de39e4da5de41e838d8e0a877192efd23dc50292
cheiy <gschege@gmail.com>
2022-11-12 00:25:22 +0000

    realloc

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _realloc - Function reallocates a memory block using malloc and free.
+ * @ptr: Pointer to the memory previously allocated
+ * @old_size: Size, in bytes, of the allocated space for ptr.
+ * @new_size: New size, in bytes, of the new memory block.
+ *
+ * Description: Function reallocates a memory block using malloc and free.
+ *
+ * Return: Pointer to the reallocated memory.
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *ptr2;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (ptr == NULL)
+	{
+		ptr = malloc(new_size - old_size);
+		if (ptr == NULL)
+			return (NULL);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (new_size > old_size)
+	{
+		ptr2 = malloc(new_size - 0);
+		if (ptr2 == NULL)
+			return (NULL);
+		free(ptr);
+	}
+	return (ptr2);
+}
---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Program multiplies two positive numbers
+ * @argc: Number of arguments passed to our program.
+ * @argv: Array of pointers to our arguments.
+ *
+ * Description: Program multiples two positive numbers in base 10
+ *
+ * Return: Nothing
+ */
+int main(int argc, char *argv[])
+{
+	unsigned long long result;
+	unsigned long long num1;
+	unsigned long long num2;
+
+	if (argc < 3 || argc > 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoll(argv[1]);
+	num2 = atoll(argv[2]);
+
+	result = num1 * num2;
+
+	printf("%llu\n", result);
+
+	return (0);
+}
