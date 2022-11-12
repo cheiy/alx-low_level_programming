89836abb7aa5939abdc61c3237d7eed157c790c0
cheiy <gschege@gmail.com>
2022-11-12 00:32:07 +0000

    _realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -19,9 +19,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (ptr);
 	if (ptr == NULL)
 	{
-		ptr = malloc(new_size - old_size);
+		ptr2 = malloc(new_size);
 		if (ptr == NULL)
 			return (NULL);
+		free(ptr);
 	}
 	if (new_size == 0 && ptr != NULL)
 	{
