4950ae07fadbe15b207abd390ca18aa5341716ea
cheiy <gschege@gmail.com>
2022-11-16 01:50:12 +0000

    New_dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,8 +15,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *dog;
 
 	dog = malloc(sizeof(dog_t));
-	if (dog == NULL || name == NULL || owner || NULL)
+	if (dog == NULL)
 		return (NULL);
+	if (name == NULL || owner == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
 	dog->name = name;
 	dog->age = age;
 	dog->owner = owner;
