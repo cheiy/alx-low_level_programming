cd6c88dead2cc68da847e7b03673abee4f8a061a
cheiy <gschege@gmail.com>
2022-11-16 00:45:58 +0000

    Init dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -14,7 +14,7 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL || name == NULL || owner == NULL)
+	if (d == NULL || name == NULL || age == 0 || owner == NULL)
 		exit(1);
 	d->name = name;
 	d->age = age;
