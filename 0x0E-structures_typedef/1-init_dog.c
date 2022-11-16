51503aef5b3de98c120ff8d374e5736c63ec442f
cheiy <gschege@gmail.com>
2022-11-15 21:35:33 -0800

    Init dog correction

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -14,9 +14,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL || name == NULL || age == 0 || owner == NULL)
-		exit(EXIT_FAILURE);
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d != NULL && name != NULL && owner != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
