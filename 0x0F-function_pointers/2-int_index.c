c30713f7ece2d6be1a1105a7946c601c141b0636
cheiy <gschege@gmail.com>
2022-11-17 15:04:11 +0000

    Int Index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,16 +14,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int ret_val;
 
 	i = 0;
-	if (size <= 0)
-		return (-1);
 	if (array == NULL || cmp == NULL)
 	{
 		exit(EXIT_FAILURE);
 	}
-	/*if (size <= 0)
+	if (size <= 0)
 	{
 		return (-1);
-	}*/
+	}
 	while (i < size)
 	{
 		if (cmp(array[i]) != 0)
