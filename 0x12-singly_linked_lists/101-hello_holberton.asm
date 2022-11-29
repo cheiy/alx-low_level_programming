df4e8c7f6036f16db9866ce78e407d47c73ce06a
cheiy <gschege@gmail.com>
2022-11-29 13:28:55 +0000

    Bits! Assemble!

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,13 +1,12 @@
 	bits 64
 	global main
-	extern printf
 
 	section .txt
 main:
+    extern printf
     mov rdi, str
     mov al,0
     call printf
 ret
-
 str:
-     db `Hello, Holberton\n`,0
+    db `Hello, Holberton\n`,0
