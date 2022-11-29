6e732421643d31129620856ed64a023d64ebbdce
cheiy <gschege@gmail.com>
2022-11-29 03:29:51 +0000

    add_node_end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -12,6 +12,7 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node;
+	list_t *last_node;
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
@@ -19,10 +20,19 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (NULL);
 	}
 	new_node->str = strdup(str);
+	new_node->len = strlen(str);
 	new_node->next = NULL;
-	if ((*head) == NULL)
+	if (*head == NULL)
+		*head = new_node;
+	else
 	{
-		(*head) = new_node;
+		last_node = *head;
+		while (last_node->next != NULL)
+		{
+			last_node = last_node->next;
+		}
+		last_node->next = new_node;
 	}
-	return (new_node);
+
+	return (last_node);
 }
