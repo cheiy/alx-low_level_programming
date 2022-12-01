4aad86b8ee6c54b49ddac88ec0dc58e1a590e91d
cheiy <gschege@gmail.com>
2022-12-01 08:28:25 +0000

    Elements

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * print_listint - Function prints all the elements of a listint_t list.
+ * @h: Pointer to the head node.
+ *
+ * Description: Function prints all the elements of a listint_t list.
+ *
+ * Return: The number of nodes.
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t i;
+	const listint_t *current;
+
+	i = 0;
+	current = h;
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		i++;
+		if (current->next == NULL)
+			break;
+		current = h->next;
+	}
+	return (i);
+}
