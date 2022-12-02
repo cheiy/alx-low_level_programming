186c9e2adea6a81150d0aa68fef7afc20ae26ab9
cheiy <gschege@gmail.com>
2022-12-02 23:22:38 +0000

    Delete_node_correction

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -38,7 +38,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 			{
 				temp = current->next;
 				current->next = current->next->next;
-				break;
+				return (1);
 			}
 			else if (index > idx)
 			{
