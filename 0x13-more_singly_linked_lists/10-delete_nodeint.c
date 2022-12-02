9d3754087f53ede8803ea9176a9b6f4f988ebd1b
cheiy <gschege@gmail.com>
2022-12-02 00:56:24 +0000

    Insert_delete

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,53 @@
+#include "lists.h"
+/**
+ * delete_nodeint_at_index - Function deletes the node at given index of list.
+ * @head: Pointer to pointer to the head node.
+ * @index: The index of the node to delete.
+ *
+ * Description: Function deletes the node at given index of linked list.
+ *
+ * Return: 1 if successful, -1 if it fails.
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *temp, *current;
+	unsigned int idx, i;
+	int retval;
+
+	i = idx = 0;
+	temp = (*head);
+	if (temp == NULL)
+		retval = -1;
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+		idx++;
+	}
+	if (index == 0)
+	{
+		temp = (*head);
+		(*head) = (*head)->next;
+		return (1);
+	}
+	else
+	{
+		current = *head;
+		while (current != NULL)
+		{
+			i++;
+			if (index == i)
+			{
+				temp = current;
+				free(temp);
+				return (1);
+			}
+			else if (index > idx)
+			{
+				return (-1);
+			}
+			current = current->next;
+		}
+	}
+	return (retval);
+}
+
---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,53 @@
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - Function inserts a new node at a given position.
+ * @head: Pointer to the head node.
+ * @idx: Index in which to insert the new node.
+ * @n: The value to insert in node.
+ *
+ * Description: Function inserts a new node at a given position.
+ *
+ * Return: Address of the new node, or NULL if it fails
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int count, i;
+	listint_t *new_node, *current_node;
+
+	count = i = 0;
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	if (*head == NULL)
+		count = 0;
+	current_node = *head;
+	while (current_node != NULL)
+	{
+		current_node = current_node->next;
+		count++;
+	}
+	new_node->n = n;
+	if (idx == 0)
+	{
+		new_node->next = (*head);
+		*head = new_node;
+	}
+	else
+	{
+		current_node = *head;
+		while (current_node != NULL)
+		{
+			if (idx == i - 1)
+			{
+				printf("Index same as i\n");
+				current_node = new_node;
+				break;
+			}
+			else if (idx > count)
+				return (NULL);
+			current_node = current_node->next;
+			i++;
+		}
+	}
+	return (current_node);
+}
