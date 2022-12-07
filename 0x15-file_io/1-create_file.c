039501f0f9f558eb435a77df58db042a67f685cb
cheiy <gschege@gmail.com>
2022-12-07 23:40:35 +0000

    Create_file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * create_file - Function creates a file
+ * @filename: The name of the file to create
+ * @text_content: NULL terminated string to write to the file.
+ *
+ * Description - Function creates a file.
+ *
+ * Return: 1 on success, -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, len;
+
+	len = 0;
+	fd = open(filename, O_WRONLY | O_CREAT, 0600);
+
+	if (filename == NULL)
+		return (-1);
+	if (fd < 0)
+		return (-1);
+	if (text_content == NULL)
+	{
+		fd = open(filename, O_WRONLY | O_CREAT, 0600);
+		close(fd);
+		return (1);
+	}
+	close(fd);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd < 0)
+		return (-1);
+
+	while (text_content[len] != '\0')
+	{
+		len++;
+	}
+	write(fd, text_content, len);
+	close(fd);
+
+	return (1);
+}
+
+
+
