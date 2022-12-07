dbe1e4dc8e7b8205be10ea8f710aadf256714103
cheiy <gschege@gmail.com>
2022-12-07 23:55:58 +0000

    Main_h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,7 +1,8 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 #include <stdlib.h>
-#include <ctype.h>
+#include <fcntl.h>
+#include <unistd.h>
 void _putchar(char ch);
 
 ssize_t read_textfile(const char *filename, size_t letters);
