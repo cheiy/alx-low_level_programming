17d3e31616882c5e3a37e35e8ede20043dbafa93
cheiy <gschege@gmail.com>
2022-12-08 00:14:24 +0000

    Create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,21 +14,22 @@ int create_file(const char *filename, char *text_content)
 	int fd, len;
 
 	len = 0;
-	fd = open(filename, O_WRONLY | O_CREAT, 0600);
 
 	if (filename == NULL)
 		return (-1);
-	if (fd < 0)
-		return (-1);
 	if (text_content == NULL)
 	{
-		fd = open(filename, O_WRONLY | O_CREAT, 0600);
+		fd = open(filename, O_CREAT, 0600);
 		close(fd);
 		return (1);
 	}
+
+	fd = open(filename, O_CREAT, 0600);
+	if (fd < 0)
+		return (-1);
 	close(fd);
 
-	fd = open(filename, O_WRONLY | O_APPEND);
+	fd = open(filename, O_WRONLY);
 	if (fd < 0)
 		return (-1);
 
@@ -41,6 +42,3 @@ int create_file(const char *filename, char *text_content)
 
 	return (1);
 }
-
-
-
