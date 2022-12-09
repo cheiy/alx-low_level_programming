97c70f83f165570fbdef671f61e5eafa8cebbd0f
cheiy <gschege@gmail.com>
2022-12-09 17:47:16 +0000

    Removed addition of \n character

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,19 +12,20 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
 
-	if (filename == NULL)
-		return (-1);
-	fd = open(filename, O_WRONLY | O_APPEND);
-	if (text_content == NULL && fd < 0)
-		return (-1);
-	if (text_content == NULL)
+	if (filename != NULL)
+	{
+		fd = open(filename, O_WRONLY | O_APPEND);
+		if (text_content == NULL && fd < 0)
+			return (-1);
+		if (text_content == NULL)
+			return (1);
+		if (fd < 0)
+			return (-1);
+		dprintf(fd, "%s", text_content);
+		close(fd);
 		return (1);
-	if (fd < 0)
+	}
+	else
 		return (-1);
-	dprintf(fd, "%s", text_content);
-	dprintf(fd, "%c", '\n');
-	close(fd);
-
-	return (1);
 }
 
