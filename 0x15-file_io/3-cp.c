ba01057ebc02680bf47e469565b9f403a93072a2
cheiy <gschege@gmail.com>
2022-12-09 21:44:01 +0000

    copy

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,92 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+/**
+ * main - Program copies the content of a file to another file.
+ * @argc: The number of arguments passed to the program.
+ * @argv: A pointer to an array holding the arguments passed.
+ *
+ * Description: Program copies the content of a file to another file.
+ *
+ * Return: 0 if successful, 1 otherwise.
+ */
+int main(int argc, char *argv[])
+{
+	int fd_src, fd_dst, close_src, close_dst;
+	char *error_src, *error_dst, *src_content;
+	unsigned int len;
+
+	len = 0;
+	error_src = "Error: Can't read from file ";
+	error_dst = "Error: Can't write to ";
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	else
+	{
+		fd_src = open(argv[1], O_RDONLY);
+		fd_dst = open(argv[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
+		if (fd_src < 0)
+		{
+			dprintf(STDERR_FILENO, "%s%s\n", error_src, argv[1]);
+			exit(98);
+		}
+		if (fd_dst < 0)
+		{
+			dprintf(STDERR_FILENO, "%s%s\n", error_dst, argv[2]);
+			exit(99);
+		}
+		len = str_len(argv[1]);
+		src_content = malloc(sizeof(char) * len);
+		if (src_content == NULL)
+			return (-1);
+		read(fd_src, src_content, len);
+		src_content[len + 1] = '\0';
+		len = 0;
+		while (src_content[len] != '\0')
+		{
+			dprintf(fd_dst, "%c", src_content[len]);
+			dprintf(STDOUT_FILENO, "%c", src_content[len]);
+			len++;
+		}
+		close_src = close(fd_src);
+		close_dst = close(fd_dst);
+		if (close_src != 0)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd_src);
+			exit(100);
+		}
+		if (close_dst != 0)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't close f %d", fd_dst);
+			exit(100);
+		}
+		free(src_content);
+		return (0);
+	}
+}
+/**
+ * str_len - Function gets the length of a string.
+ * @str: The string whose length we are to calculate.
+ * Description: Function gets the length of a string.
+ *
+ * Return: The length of the string
+ */
+unsigned int str_len(char str[])
+{
+	unsigned int len;
+	int fd;
+
+	len = 0;
+	fd = open(str, O_RDONLY);
+	if (fd < 0)
+		exit(EXIT_FAILURE);
+	len = lseek(fd, 0L, SEEK_END);
+	return (len);
+}
