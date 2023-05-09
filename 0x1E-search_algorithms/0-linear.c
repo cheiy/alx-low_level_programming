2a6ccc5cacdda6592c5f6910068edee8dfe07998
cheiy <gschege@gmail.com>
2023-05-09 21:26:22 +0000

    Linear

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -18,7 +18,7 @@ int linear_search(int *array, size_t size, int value)
 		return (-1);
 	if (array == NULL)
 		return (-1);
-	for (i = 0; i <= size; i++)
+	for (i = 0; i < size ; i++)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
@@ -26,11 +26,8 @@ int linear_search(int *array, size_t size, int value)
 			result = i;
 			break;
 		}
-		else if (i >= size)
-		{
+		else
 			result = -1;
-			break;
-		}
 	}
 	return (result);
 }
